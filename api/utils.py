0b41973ede2142b9cf163bf7fc600747f8a4e4e2
odydrums <oscar125_@outlook.com>
2014-04-09 12:00:28 -0500

    la erga

--- api/utils.py
+++ api/utils.py
@@ -66,7 +66,7 @@ class ResellerContract:
        	return {success:True}
 
         #authentication_token
-     def get_resellerUsers( self, snid, taxpayer_id, contract):
+     def get_resellerUsers( self, snid):
 
      	try:
      		reseller = Reseller.objects.get( id=snid)
@@ -76,19 +76,16 @@ class ResellerContract:
 
      	  if resseler.type == 'U':
      	  	try:
-     	  		reseller_user == ResellerUser.objects.filter(reseller = reseller, taxpayer_id= taxpayer_id, name = name, telephone= telephone, address= address, email= email, estado = estado)
+     	  		reseller_user == ResellerUser.objects.filter(snid = snid)
             except:
             	self.is_valid = False
             	return
           elif reseller.type == 'I':
           	try:
-          		reseller_user = ResellerUserInvoice.objects.filter(reseller=reseller, taxpayer_id= taxpayer_id, snid = snid, name = name, telephone= telephone, address= address, email= email, estado = estado)
+          		reseller_user = ResellerUserInvoice.objects.filter(snid = snid)
             except:
                 self.is_valid = False
                 retunr
-            reseller_user.contract = contract
-            reseller_user.save()
-            return {success:True}		
 
 
 

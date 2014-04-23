475c007644be733fa856343118da2de353a6d133
pxiol <pabliitomartinez@outlook.com>
2014-04-23 16:53:57 -0500

    reload

--- api/views.py
+++ api/views.py
@@ -5,39 +5,30 @@ logging.basicConfig()
 
 from spyne.model.primitive import String
 from spyne.model.primitive import Boolean
-from spyne.model.complex import Array
-from spyne.model.complex import ComplexModel
+#from spyne.model.complex import Array
+#from spyne.model.complex import ComplexModel
 from spyne.service import ServiceBase
 from spyne.decorator import srpc
-#from spyne.model.binary import File
-from spyne.model.primitive import Unicode
+#from spyne.model.primitive import Unicode
 from api.models import Reseller
-from django.conf import settings
+#from django.conf import settings
 
 class ResellerContractSOAP(ServiceBase):
-		
-	@srpc(String (encoding='utf8'),String(encoding='utf8'),String(encoding='utf8'),String(enconding='utf8'), Boolean(encoding='utf8'))
-	def sign (self, snid, taxpayer_id, authentication_token, contract):    #verificar si va True el contract.
-
-		   self.is_valid = False
-		   return
-
-#    @srpc(String(encoding='utf8'),String(encoding='utf8'),String(encoding='utf8'),String(encoding='utf8'), _returns=UserLCOResult)
-#	def verify_user (self, snid, authentication_token, taxpayer_id):
-
-#		   self.is_valid = False
-#           return   
-
-#    @srpc(String(encoding='utf8'),String(encoding='utf8'),String(encoding='utf8'), _returns=InfoLCOResult)   
-#	def get_info (self, snid, contract): 
-#
-#		   self.is_valid = False
-#          return 
-
-#    @srpc(String(encoding='utf8'),String(encoding='utf8'),String(encoding='utf8'), _returns=ResellerUsersCOResult)
-#    def get_resellerUsers (self, authentication_token, snid):
-
-#    	   self.is_valid = False
-#     	   return  
-
 	
+	@srpc(String(encoding='utf8'), String(encoding='utf8'), String(encoding='utf8'), Boolean)
+	def sign_contrat (self, snid, taxpayer_id, contract = True):
+		self.is_valid = False
+		 #return
+
+	@srpc(String(encoding='utf8'), String(encoding='utf8'), String(encoding='utf8'), String(encoding='utf8'))
+	def verify_user (self, snid, authentication_token, taxpayer_id):
+		self.is_valid = False
+      #return   
+
+	@srpc(String(encoding='utf8'), String(encoding='utf8'))   
+	def get_info (self, snid):
+		self.is_valid = False
+
+	@srpc(String(encoding='utf8'), String(encoding='utf8'), String(encoding='utf8'))
+	def get_resellerUsers (self, authentication_token, snid):
+		self.is_valid = False
